9911a3b5b01d915998b64cf8053b53e7b2664bf9
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2016-11-22 21:07:49 -0500

    (SIMP-2111) Synchronize with the 'master' changes (#11)
    The 5.X branch got out of sync with 'master' and this pulls in all of
    the appropriate changes.
    
    SIMP-2111 #close

--- manifests/service.pp
+++ manifests/service.pp
@@ -23,18 +23,9 @@
 #
 #
 class haveged::service (
-  $service_name   = defined('$::haveged::service_name') ? {
-    true => getvar('::haveged::service_name'),
-    default => $::haveged::params::service_name
-  },
-  $service_ensure = defined('$::haveged::_service_ensure') ? {
-    true => getvar('::haveged::_service_ensure'),
-    default => 'running'
-  },
-  $service_enable = defined('$::haveged::_service_enable') ? {
-    true => getvar('::haveged::_service_enable'),
-    default => true
-  }
+  $service_name   = defined('$::haveged::service_name') ? { true => getvar('::haveged::service_name'), default => $::haveged::params::service_name },
+  $service_ensure = defined('$::haveged::_service_ensure') ? { true => getvar('::haveged::_service_ensure'), default => 'running' },
+  $service_enable = defined('$::haveged::_service_enable') ? { true => getvar('::haveged::_service_enable'), default => true }
 ) inherits ::haveged::params {
 
   service { $service_name:
