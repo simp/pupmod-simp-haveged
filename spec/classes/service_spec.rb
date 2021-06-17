fac3f9cebb85f21192b1174a9f8227aa6f844743
op-ct <chris.tessmer@onyxpoint.com>
2021-06-17 12:19:23 -0400

    (SIMP-9780) Puppetsync: Remove Puppet 5, Support 7 (#51)
    * (SIMP-9780) Puppetsync: Remove Puppet 5, Support 7
    
    This patch:
    
      * Removes Puppet 5 from `metadata.json` and the GHA & GLCI matrix
      * Ensures support for Puppet 7 in `metadata.json`
        * Ensures Pup 7 for module deps stdlib and concat (if present)
      * Updates GLCI and GHA matrix to model Puppet PE using Puppet 6.22.1
      * Bumps Gemfile's simp-rake-helpers and simp-beaker-helpers min vers
      * Removes `.pmtignore` and adds `.pdkignore`
    
    The patch enforces a standardized asset baseline using simp/puppetsync,
    and may apply other updates to ensure conformity.
    
    [SIMP-9805] #close
    [SIMP-9780] #comment Drop Puppet 5 in pupmod-simp-haveged
    [SIMP-9670] #comment update Puppet 6.22.1 in pupmod-simp-haveged
    [SIMP-9606] #comment Switch pupmod-simp-haveged from .pmtignore to .pdkignore
    [SIMP-9781] #comment bump Gemfile versions in pupmod-simp-haveged
    
    Co-authored-by: lnemsick-simp <lnemsick-simp@users.noreply.github.com>

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -2,60 +2,70 @@ require 'spec_helper'
 
 describe 'haveged::service' do
   on_supported_os.each do |os, os_facts|
-    let :facts  do
-      os_facts
-    end
+    context "on #{os}" do
+      let :facts  do
+        os_facts
+      end
 
-    context 'with default parameters' do
-      it {
-        is_expected.to contain_service('haveged')
-          .with_ensure('running')
-          .with_enable(true)
-      }
-
-      context 'with rngd running' do
-        let :facts do
-          os_facts.merge({
-            :haveged__rngd_enabled => true
-          })
+      context 'with default parameters' do
+        let(:expected_ensure) do
+          facts[:os][:family] == 'RedHat' && facts[:os][:release][:major].to_i < 8 ? 'running' : 'stopped'
         end
 
+        let(:expected_enable) do
+          facts[:os][:family] == 'RedHat' && facts[:os][:release][:major].to_i < 8 ? true : 'mask'
+        end
         it {
           is_expected.to contain_service('haveged')
-            .with_ensure('stopped')
-            .with_enable('mask')
+            .with_ensure(expected_ensure)
+            .with_enable(expected_enable)
         }
 
-        context 'with haveged forced' do
-          let :params do
-            {
-              :force_if_rngd_running => true
-            }
+        context 'with rngd running' do
+          let :facts do
+            os_facts.merge({
+              :haveged__rngd_enabled => true
+            })
           end
 
           it {
             is_expected.to contain_service('haveged')
-              .with_ensure('running')
-              .with_enable(true)
+              .with_ensure('stopped')
+              .with_enable('mask')
           }
+
+          context 'with haveged forced' do
+            let :params do
+              {
+                :force_if_rngd_running => true
+              }
+            end
+
+            it {
+              is_expected.to contain_service('haveged')
+                .with_ensure('running')
+                .with_enable(true)
+            }
+          end
         end
       end
-    end
 
-    context 'with defined parameters' do
-      let :params do
-        {
-          :service_name   => 'foobar',
-          :service_enable => true,
-          :service_ensure => 'running'
+      context 'with defined parameters' do
+        let :params do
+          {
+            :service_name   => 'foobar',
+            :service_enable => true,
+            :service_ensure => 'running',
+            :force_if_rngd_running => true
+          }
+        end
+
+        it {
+          is_expected.to contain_service(params[:service_name])
+            .with_ensure(params[:service_ensure])
+            .with_enable(params[:service_enable])
         }
       end
-
-      it {
-        is_expected.to contain_service(params[:service_name])
-          .with_ensure(params[:service_ensure])
-          .with_enable(params[:service_enable])
-      }
     end
   end
 end
