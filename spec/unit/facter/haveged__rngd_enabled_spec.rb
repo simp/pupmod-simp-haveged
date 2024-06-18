85ec9994cb0a1eeb3c3e149dcb36c6883d3dfc5b
Steven Pritchard <steve@sicura.us>
2024-06-18 15:32:19 -0500

    Switch from mocha to rspec mocks

--- spec/unit/facter/haveged__rngd_enabled_spec.rb
+++ spec/unit/facter/haveged__rngd_enabled_spec.rb
@@ -7,7 +7,7 @@ describe 'Facter::Util::Fact' do
 
   before(:each) do
     Facter.clear
-    Facter::Util::Resolution.stubs(:which).with('pgrep').returns(pgrep_path)
+    allow(Facter::Util::Resolution).to receive(:which).with('pgrep').and_return(pgrep_path)
   end
 
   after(:each) do
@@ -16,14 +16,14 @@ describe 'Facter::Util::Fact' do
 
   context 'with rngd running' do
     it do
-      Facter::Core::Execution.stubs(:execute).with("#{pgrep_path} rngd", on_fail: false).returns("1234\n")
+      allow(Facter::Core::Execution).to receive(:execute).with("#{pgrep_path} rngd", on_fail: false).and_return("1234\n")
       expect(Facter.fact(:haveged__rngd_enabled).value).to eq(true)
     end
   end
 
   context 'with rngd stopped' do
     it do
-      Facter::Core::Execution.stubs(:execute).with("#{pgrep_path} rngd", on_fail: false).returns("\n")
+      allow(Facter::Core::Execution).to receive(:execute).with("#{pgrep_path} rngd", on_fail: false).and_return("\n")
       expect(Facter.fact(:haveged__rngd_enabled).value).to eq(false)
     end
   end
