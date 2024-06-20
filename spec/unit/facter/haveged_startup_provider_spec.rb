216f83aca62e9125ccc8d82413a3095fda3e693d
Steven Pritchard <steven.pritchard@gmail.com>
2024-06-20 10:40:28 -0500

    Switch from mocha to rspec mocks (#79)
    * Switch from mocha to rspec mocks
    * Remove GitLab CI hooks from GitHub Actions

--- spec/unit/facter/haveged_startup_provider_spec.rb
+++ spec/unit/facter/haveged_startup_provider_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe 'Facter::Util::Fact' do
   before(:each) do
     Facter.clear
-    Facter.fact(:kernel).stubs(:value).returns('Linux')
+    allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
   end
 
   after(:each) do
@@ -14,14 +14,14 @@ describe 'Facter::Util::Fact' do
 
   context 'when haveged_startup_provider is /proc/1/comm' do
     it {
-      File.stubs(:open).returns("foo\n")
+      allow(File).to receive(:open).and_return("foo\n")
       expect(Facter.fact(:haveged_startup_provider).value).to eq('foo')
     }
   end
 
   context 'when haveged_startup_provider raises and exception' do
     it {
-      File.stubs(:open) { raise(StandardException) }
+      allow(File).to receive(:open) { raise(StandardException) }
       expect(Facter.fact(:haveged_startup_provider).value).to eq('init')
     }
   end
